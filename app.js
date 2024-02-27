dc00215957fca9fe7d6361c6e27734dada3bf83d
oLNidfwworld <kuvetov@bk.ru>
2024-02-27 19:01:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const webpush = require('web-push');
+const cors = require("cors")
+
+const port = 3000;
+
+const apiKeys = {
+    publicKey: "PUBLIC_KEY",
+    privateKey: "PUBLIC_KEY"
+}
+  
+
+webpush.setVapidDetails(
+    'mailto:mail@mail.ru',
+    apiKeys.publicKey,
+    apiKeys.privateKey
+)
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+const subDatabse = [];
+
+
+app.post("/save-subscription", (request, response) => {
+    subDatabse.push(request.body);
+    response.json({ status: "Success", message: "Subscription saved!" })
+})
+
+app.get("/send-notification", (req, res) => {
+    webpush.sendNotification(subDatabse[0], "Hello world");
+    res.json({ "statue": "Success", "message": "Message sent to push service" });
+})
+
+app.listen(port, () => {
+    console.log("Server running on port 3000!");
+})
